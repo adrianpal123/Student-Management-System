5d03fc4548cc3425cc17cb41b24b5149f8701a42
Adrian <adrian.pal00@e-uvt.ro>
2021-10-20 17:51:33 +0300

    Front-end; First commit

---
+++ src/app/student.ts
@@ -0,0 +1,12 @@
+export interface Student
+{
+    id: number;
+    name: string;
+    email: string;
+    phoneNumber: string;
+    imageUrl : String;
+    studentCode: string;
+    year: number;
+    department: string;
+    
+}
